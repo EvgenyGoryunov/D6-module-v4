0bf8483eeba90efc3397ee50edcfa7b844476f15
EvgenyGoryunov <89795937+EvgenyGoryunov@users.noreply.github.com>
2022-01-13 23:08:52 +0700

    6.5 работа со списками для рассылки 3 - база закачена для подписчиков для тестов 6.5 работа со списками для рассылки 3 6.5 работа со списками для рассылки 2 6.5 работа со списками для рассылки 1

--- news_app/appointments/views.py
+++ news_app/appointments/views.py
@@ -6,15 +6,12 @@ from django.views import View
 from newapp.models import Category, Post
 from .models import Appointment
 
-import datetime
-
 
 # мои тесты
 class AppointView(View):
 
     def get(self, request, *args, **kwargs):
         return render(request, 'test_test.html')
-
 
     def post(self, request):
         pole_test = request.POST['test']
@@ -23,36 +20,27 @@ class AppointView(View):
         else:
             pole_test = 1
 
-
         pole_test2 = request.POST['test2']
         if pole_test2:
             pole_test2 = pole_test2
         else:
             # предыдущия неделя находится так
-            pole_test2 = datetime.datetime.now().isocalendar()[1]-1
-
-
+            pole_test2 = datetime.now().isocalendar()[1] - 1
 
         pole_spisok1 = Category.objects.get(pk=pole_test)
-
 
         pole_spisok2 = Category.objects.get(pk=pole_test).subscribers.all()
         for qwe in pole_spisok2:
             pass
 
-
-
-        pole_spisok3 = Post.objects.filter(category_id=pole_test, dateCreation__week=pole_test2).values('pk', 'title', 'dateCreation')
+        pole_spisok3 = Post.objects.filter(category_id=pole_test, dateCreation__week=pole_test2).values('pk', 'title',
+                                                                                                        'dateCreation')
         # pole_spisok3 = Post.objects.filter(category_id=pole_test, author_id=pole_test2).values('pk', 'title', 'dateCreation')
-
-
-
 
         news_from_category = []
         for news in pole_spisok3:
             new = (f'{news.get("title")}, {news.get("dateCreation")}, http://127.0.0.1:8000/news/{news.get("pk")}')
             news_from_category.append(new)
-
 
         return render(request, 'test_test.html', {
             'pole_test_html': pole_test,
@@ -63,12 +51,10 @@ class AppointView(View):
             "news_by_category": news_from_category,
         })
 
-
         # qaz = Post.objects.filter(category_id=pole_test).values('pk').get("pk")
         # print('qaz:', qaz)
         # wsx = Post.objects.filter(category_id=pole_test).values('title').get("title")
         # print('wsx:', wsx)
-
 
         # print('pole_spisok3:', pole_spisok3)
         # print('Список статей на данную тему:')
